0a8a715cdc7f1afa0d71c0ea8d1f4ed79e8b40e5
Sumeet Jadhav <sumeet.jadhav@e-zest.in>
2024-01-25 00:20:28 +0530

    removed unnecessary code from valid parenthesis code

--- code/javascript/ValidParentheses.js
+++ code/javascript/ValidParentheses.js
@@ -56,20 +56,3 @@ const isValid = (str) => {
 const result = isValid('()[]{}');
 // const result = isValid('(]');
 console.log('result: ', result);
-
-let employee = {
-  eid: 'E102',
-  ename: 'Jack',
-  eaddress: 'New York',
-  salary: 50000,
-};
-console.log('=========Deep Copy========');
-let newEmployee = JSON.parse(JSON.stringify(employee));
-
-console.log('Employee=> ', employee);
-console.log('New Employee=> ', newEmployee);
-console.log('---------After modification---------');
-newEmployee.ename = 'Beck';
-newEmployee.salary = 70000;
-console.log('Employee=> ', employee);
-console.log('New Employee=> ', newEmployee);
